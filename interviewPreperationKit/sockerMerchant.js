8ae22c85471e2fe79e63dcac91ffb13f73d762b3
bmajjiga <majjjigabharatreddy@gmail.com>
2020-06-05 11:44:21 +0530

    made few changes in socker Mercahnt

--- interviewPreperationKit/sockerMerchant.js
+++ interviewPreperationKit/sockerMerchant.js
@@ -2,23 +2,29 @@
 var n = 9;
 var arr = [10,20,20,10,10,30,50,10,20];
 
-var obj = {};
-for(var i=0; i<n; i++) {
-    var temp = arr[i];
-    if(obj[temp])
-        obj[temp]++;
-     else
-        obj[temp]=1;
+function sockMerchant(n, arr) {
+    
+    if(n ==0)
+        return 0;
+        
+    var obj = {};
+    for(var i=0; i<n; i++) {
+        var temp = arr[i];
+        if(obj[temp])
+            obj[temp]++;
+        else
+            obj[temp]=1;
+    }
+
+    console.log(obj);
+
+    var count=0;
+    var objvalues = Object.values(obj);
+    for(var i=0; i<objvalues.length; i++)
+    {
+        count+= parseInt(objvalues[i]/2);
+    }
+    return count;
 }
 
-console.log(obj);
-
-var count=0;
-var objvalues = Object.values(obj);
-for(var i=0; i<objvalues.length; i++)
-{
-    count+= parseInt(objvalues[i]/2);
-}
-
-console.log(count);
-
+console.log(sockMerchant(n, arr));
